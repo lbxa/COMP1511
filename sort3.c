68b2552ea3163e0b336581b8e01b0705bcb7e0b3
lucasbrsa <lucas.chu.barbosa@gmail.com>
2017-03-22 18:32:26 +1100

    Added sort function

---
+++ sort3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void sort(int first, int second, int third);
+
+int main(void) { 
+
+	int first, second, third; 
+	scanf ("%d %d %d", &first, &second, &third);
+	sort(first, second, third);
+
+	return EXIT_SUCCESS;
+
+}
+
+void sort(int first, int second, int third) {
+	int array[3];
+	array[0] = first;
+	array[1] = second;
+	array[2] = third;
+
+	for (int i = 0; i < 3; i++) {
+		int key = array[i];
+		int previousElement = i - 1;
+
+		while ((previousElement >= 0) && (array[previousElement] > key)) {
+			array[previousElement + 1] = array[previousElement];
+			previousElement -= 1;
+		}
+		
+		array[previousElement + 1] = key;
+		
+	}
+
+	for (int i = 0; i < 3; i++) {
+		printf("%d\n", array[i]);
+	}
+}
