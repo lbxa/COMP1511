45b1073c51c9d8a820952134103f370c0dd3f0e2
Lucas Barbosa <Lucas Barbosa>
2017-03-23 02:22:31 +1100

    Uploaded new lecture files

--- build/main.c
+++ build/main.c
@@ -1,36 +1,94 @@
+// Lucas Barbosa
+// 22.03.17
+// Memory exploration
+
 #include <stdio.h>
 #include <stdlib.h>
-
-#define TRUE 1
-#define FALSE 0
-
-// PROTOTYPES & STUBS
-
-void swap(int *x, int *y);
-
-int main(void) 
-{
-	
-  int x = 10;
-	int y = 3;
-	
-	printf("%d, %d\n", x, y);
-	swap(&x, &y);
-	printf("%d, %d\n", x, y);
-    
-	return 0;
-	
+ 
+long add (int x, int y);
+ 
+int main(int argc, const char * argv[]) {
+ 
+   int x;
+   int y;
+   long total;
+ 
+   x = 40;
+   y = 2;
+ 
+   total = add (x, y);
+ 
+   printf("the sum of %d and %d is %ld\n", x, y, total);
+   printf("\n\n\n\n");
+ 
+   char c ='a';
+ 
+   unsigned long ul       = 0;
+   unsigned int ui        = 1;
+   unsigned long long ull = 2;
+   unsigned short us      = 3;
+   
+   // printf() table formating with "%-[CELLWIDTH]"
+   
+   printf("The Stack\n\n");
+   
+   printf("%-25s%-20s%-18s\n", "[Unsigned Type]", "Memory (bytes)", 
+   "Stack location");
+   printf("%-25s%-20d0x%-18p\n", "Long", sizeof(ul), &ul);
+   printf("%-25s%-20d0x%-18p\n", "Int", sizeof(ui), &ui);
+   printf("%-25s%-20d0x%-18p\n", "Long Long", sizeof(ull), &ull);
+   printf("%-25s%-20d0x%-18p\n", "Short", sizeof(us), & us);
+   printf("\n");
+   
+   signed long sl       = 4;
+   signed int si        = 5;
+   signed long long sll = 6;
+   signed short ss      = 7;
+   
+   printf("%-25s%-20s%-18s\n", "[Signed Type]", "Memory (bytes)", 
+   "Stack location");
+   printf("%-25s%-20d0x%-18p\n", "Long", sizeof(sl), &sl);
+   printf("%-25s%-20d0x%-18p\n", "Int", sizeof(si), &si);
+   printf("%-25s%-20d0x%-18p\n", "Long Long", sizeof(sll), &sll);
+   printf("%-25s%-20d0x%-18p\n", "Short", sizeof(ss), & ss);
+   printf("\n");
+ 
+   long l       = 8;
+   int i        = 9;
+   long long ll = 10;
+   short s      = 11;
+   
+   printf("%-25s%-20s%-18s\n", "[Generic Types]", "Memory (bytes)", 
+   "Stack location");
+   printf("%-25s%-20d0x%-18p\n", "Long", sizeof(l), &l);
+   printf("%-25s%-20d0x%-18p\n", "Int", sizeof(i), &i);
+   printf("%-25s%-20d0x%-18p\n", "Long Long", sizeof(ll), &ll);
+   printf("%-25s%-20d0x%-18p\n", "Short", sizeof(s), & s);
+   printf("\n");
+ 
+   float f = 3.1;
+   double d = 3.14;
+ 
+   // add them all together just to make use of them so the compiler
+   // doesn't grumble that they are unused in the program
+ 
+   double grandTotal;
+   grandTotal =  c +
+                 ul + ui + ull + us +
+                 sl + si + sll + ss +
+                  l +  i +  ll +  s +
+                  f + d;
+ 
+   
+   printf("\n\n\n\n");
+   printf ("all these things added together make %f\n", grandTotal);
+ 
+   return EXIT_SUCCESS;
 }
-
-// FUNCTIONS & DRIVERS
-
-void swap(int *x, int *y) {
-
-	int *tmp;
-  *tmp = *x;
-	*x = *y;
-	*y = *tmp;
-	
+ 
+long add (int x, int y) {
+   long answer;
+   answer = x + y;
+ 
+   return answer;
 }
-
-
