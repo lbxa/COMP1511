c91af42157dc60eee056fa11b3705d41dc1c86a3
Lucas Barbosa <Lucas Barbosa>
2017-03-25 04:37:48 +1100

    Added aditional files

---
+++ build/main.c
@@ -0,0 +1,40 @@
+//
+//
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int printWondrous (int start);
+
+int main(int argc, char **argv) {
+   
+   
+   
+   return EXIT_SUCCESS;
+   
+}
+
+int printWondrous (int start) {
+   
+   /*
+    *  counter = 0;
+    *
+    *  WHILE counter <> 1
+    *    IF number is even THEN
+    *       next term = half the current term
+    *    ENDIF
+    *    IF number is odd THEN
+    *       next term = 3 timers current term plus 1
+    *    ENDIF
+    *    counter = counter + 1
+    *  ENDWHILE
+    *
+    */
+   int counter = 0;
+   while () 
+   {
+      
+   }
+   
+}
