0d2c6382154d5488bb5d4e5045045bb6c54bbd7a
Lucas Barbosa <Lucas Barbosa>
2017-03-25 14:52:36 +1100

    Uploaded new source files

---
+++ lib/sortWithSwap.c
@@ -0,0 +1,45 @@
+// Lucas Barbosa
+// 23/03/17
+// Insertion sorting values via reference
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void sort(int *first, int *second, int *third);
+
+int main(int argc, char **argv) { 
+
+	int first, second, third; 
+	scanf ("%d %d %d", &first, &second, &third);
+	sort(&first, &second, &third);
+   printf("%d, %d, %d\n", first, second, third);
+
+	return EXIT_SUCCESS;
+
+}
+
+void sort(int *first, int *second, int *third) {
+	
+   int array[3];
+	array[0] = (*first);
+	array[1] = (*second);
+	array[2] = (*third);
+
+	for (int i = 0; i < 3; i++) {
+		int key = array[i];
+		int previousElement = i - 1;
+
+		while ((previousElement >= 0) && (array[previousElement] > key)) {
+			array[previousElement + 1] = array[previousElement];
+			previousElement -= 1;
+		}
+		
+		array[previousElement + 1] = key;
+		
+	}
+
+   (*first) = array[0];
+	(*second) = array[1];
+	(*third) = array[2]; 
+
+}
